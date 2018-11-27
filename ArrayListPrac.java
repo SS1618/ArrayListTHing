d826a61b6515f1f6038430363ae10dc7d9189679
SS1618 <45392178+SS1618@users.noreply.github.com>
2018-11-27 08:37:04 -0500

    Added combineList and printList functions
    combineList combines two lists and printList prints a list with commas to separate values.

--- ArrayListPrac.java
+++ ArrayListPrac.java
@@ -3,4 +3,22 @@ public class ArrayListPrac{
 	public static void main(String[]args){
 
 	}
+	public static ArrayList<Integer> combineList(ArrayList<Integer> foo, ArrayList<Integer> poo){
+		ArrayList<Integer> goo = new ArrayList<>();
+		for(int i = 0; i<foo.size(); i++){
+			goo.add(foo.get(i));
+		}
+		for(int i = 0; i<poo.size(); i++){
+			goo.add(poo.get(i));
+		}
+		return goo;
+	}
+	public static void printList(ArrayList<Integer> foo){
+		for(int i = 0; i<foo.size(); i++){
+			System.out.print(foo.get(i));
+			if(i != foo.size()-1){
+				System.out.print(", ");
+			}
+		}
+	}
 }
